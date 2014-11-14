3b1d53e78133df85c8d82806a5d705825cc55ca8
Arthur <ikeagold@users.noreply.github.com>
2014-11-14 08:45:23 +0200

    path fix

--- fs-wp-basic-template/footer.php
+++ fs-wp-basic-template/footer.php
@@ -15,7 +15,7 @@
     <div itemscope itemtype="http://schema.org/Organization">
         <span itemprop="name">FlickShine</span>
         <span><a href="http://flickshine.com" itemprop="url">flickshine.com</a></span>
-        <img itemprop="logo" alt="logo" src="http://flickshine.com/wp-content/themes/mytheme/img/logo.png" />
+        <img itemprop="logo" alt="logo" src="<?php bloginfo('template_url');?>/img/logo.png" />
         <div itemprop="address" itemscope itemtype="http://schema.org/PostalAddress">
             <strong>Postal Address:</strong>
             <span itemprop="streetAddress">10-10 44 Ave Suite # 30, 4th floor</span>
@@ -30,7 +30,7 @@
             "@context": "http://schema.org",
             "@type": "Organization",
             "url": "http://flickshine.com",
-            "logo": "http://flickshine.com/wp-content/themes/mytheme/img/logo.png",
+            "logo": "<?php bloginfo('template_url');?>/img/logo.png",
             "contactPoint": [
                 {
                     "@type": "ContactPoint",
