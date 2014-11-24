a1637b30593985333dc2e5080f6fe69293a0447b
Arthur <ikeagold@gmail.com>
2014-11-24 12:35:33 +0200

    recommend to yoast seo and etc.

--- fs-wp-basic-template/header.php
+++ fs-wp-basic-template/header.php
@@ -12,30 +12,34 @@
     <meta charset=<?php bloginfo('charset');?>>
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title><?php wp_title('|',true,'right');?></title>
-    <link rel="canonical" href="<?php bloginfo('url');?>">
     <link rel="stylesheet" href="<?php bloginfo('template_url');?>/reset.min.css">
     <link rel="stylesheet" href="<?php bloginfo('template_url');?>/style.min.css">
     <link rel="pingback" href="<?php bloginfo('pingback_url');?>">
     <link rel="icon" href="<?php bloginfo('template_url');?>/favicon.ico" type="image/x-icon">
     <!--[if IE]><link rel="shortcut icon" href="favicon.ico" type="image/x-icon"><meta http-equiv="imagetoolbar" content="no"><![endif]-->
     <!--[if lt IE 9]><meta http-equiv="X-UA-Compatible" content="IE=edge"><script src="http://cdn.jsdelivr.net/g/html5shiv,css3-mediaqueries"></script><![endif]-->
+    
+    <!-- To automatize this tags, pls use Yoast WordPress SEO plugin - https://yoast.com/wordpress/plugins/seo/
+    <link rel="canonical" href="<?php bloginfo('url');?>">
     <meta property="og:site_name" content="<?php bloginfo('name');?>">
     <meta property="og:type" content="website">
     <meta property="og:locale" content="<?php bloginfo('language');?>">
     <meta property="og:title" content="<?php wp_title('|',true,'right');?>">
     <meta property="og:description" content="<?php bloginfo('description');?>">
     <meta property="og:url" content="<?php bloginfo('url');?>">
-    <meta property="og:image" content="<?php bloginfo('template_url');?>/img/logo.png">
-    <meta property="og:keywords" content="key,words">
-    <meta property="fb:app_id" content="appid from http://findmyfacebookid.com/">
     <meta name="twitter:card" content="summary">
     <meta name="twitter:site" content="@yoursite">
     <meta name="twitter:creator" content="@FlickShine">
+    Other meta tags what not supported by Yoast WordPress SEO plugin -->
+    <meta property="og:image" content="<?php bloginfo('template_url');?>/img/logo.png">
+    <meta property="og:keywords" content="key,words">
+    <meta property="fb:app_id" content="appid from http://findmyfacebookid.com/">
     <meta name="dc.title" content="<?php wp_title('|',true,'right');?>">
     <meta name="dc.language" content="<?php bloginfo('language');?>">
     <meta name="dcterms.subject" content="<?php bloginfo('description');?>">
     <meta name="dc.description" content="<?php bloginfo('description');?>">
     <meta name="dc.creator" content="FlickShine">
+    
     <script src="https://apis.google.com/js/platform.js" async defer></script>
     <?php wp_head();?>
 </head>
